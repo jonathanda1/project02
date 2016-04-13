42fd9643f4a5b7e26d3817b9aa5cc3ee691449d5
Jonathan Dai <jonathanjddai@gmail.com>
2016-04-13 11:32:46 -0700

    backgrounds updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation, :name)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
